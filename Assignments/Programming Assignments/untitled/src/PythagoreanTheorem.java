ef45838e88de3e9d8105beaaf541f974ee42ffda
niccolosea0 <niccolosea@gmail.com>
2025-12-09 16:52:43 +0400

    Adding PythagoreanTheorem task

---
+++ Assignments/Programming Assignments/untitled/src/PythagoreanTheorem.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PythagoreanTheorem {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a first number: ");
+        int a = scanner.nextInt();
+
+       System.out.print("Enter a second number: ");
+       int b = scanner.nextInt();
+
+       double c = Math.sqrt(a + b);
+       System.out.println("Square of sum: " + c);
+
+       scanner.close();
+   }
+}
