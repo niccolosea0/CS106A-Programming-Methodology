f4c5ab4d75fb02bc81a049089a116721093d4fcd
niccolosea0 <niccolosea@gmail.com>
2025-12-26 20:03:07 +0400

    Modified programming hierarchy task

--- assignments/programming assignments/assignment2/src/ProgramHierarchy.java
+++ assignments/programming assignments/assignment2/src/ProgramHierarchy.java
@@ -36,25 +36,31 @@ public class ProgramHierarchy extends GraphicsProgram {
         // Add labels
         // top label
         GLabel program = new GLabel("Program");
+        program.setFont("Sansserif-16"); 
         double programX = cx - program.getWidth() / 2.;
         double programY = cy - (BRICK_HEIGHT / 2) + (program.getAscent() / 2.);
         add(program, programX, programY);
 
         // bottom-middle label
         GLabel console_program = new GLabel("ConsoleProgram");
+        console_program.setFont("Sansserif-16"); 
         double consoleX = cx - console_program.getWidth() / 2.; 
-        double bottomY = cy + (1.5 * BRICK_HEIGHT) + console_program.getAscent() / 2.; // This variable is same for all the bottom rectangles
+        double bottomY = cy + (1.5 * BRICK_HEIGHT) + console_program.getAscent() / 2.; 
         add(console_program, consoleX, bottomY);
 
         // bottom-left label
         GLabel graphics_program = new GLabel("GraphicsProgram");
+        graphics_program.setFont("Sansserif-16"); 
         double graphicsX = cx - (1.5 * BRICK_WIDTH) - graphics_program.getWidth() / 2.;
-        add(graphics_program, graphicsX, bottomY);
+        double graphicsY = cy + (1.5 * BRICK_HEIGHT) + console_program.getAscent() / 2.; 
+        add(graphics_program, graphicsX, graphicsY);
 
         // bottom-right label
         GLabel dialog_program = new GLabel("DialogProgram");
+        dialog_program.setFont("Sansserif-16"); 
         double dialogX = cx + (1.5 * BRICK_WIDTH) - dialog_program.getWidth() / 2.;
-        add(dialog_program, dialogX, bottomY);
+        double dialogY = cy + (1.5 * BRICK_HEIGHT) + console_program.getAscent() / 2.; 
+        add(dialog_program, dialogX, dialogY);
     }
 
     public void add_brick(double x, double y) {
