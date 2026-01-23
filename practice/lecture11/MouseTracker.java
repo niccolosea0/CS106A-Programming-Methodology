b46d5176edfa88015909351fcfbd3eb71b7ae9ba
niccolosea0 <niccolosea@gmail.com>
2026-01-23 19:51:02 +0400

    Add MouseTracker.java program

---
+++ practice/lecture11/MouseTracker.java
@@ -0,0 +1,20 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.event.*;
+
+public class MouseTracker extends GraphicsProgram {
+
+    private GLabel label;
+
+    public void run() {
+        label = new GLabel("");
+        label.setFont("Times New Roman-36");
+        add(label, 50, 50);
+
+        addMouseListeners();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        label.setLabel("Mouse: (" + e.getX() + " : " + e.getY() + ")");
+    }
+}
