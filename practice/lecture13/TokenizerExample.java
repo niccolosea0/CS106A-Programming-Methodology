99f1cf323bb88028bc2c16372149bae76e12929d
niccolosea0 <niccolosea@gmail.com>
2026-03-02 20:18:39 +0400

    Add Tokenizer example

---
+++ practice/lecture13/TokenizerExample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import acm.program.*;
+
+public class TokenizerExample extends ConsoleProgram {
+
+    private void printTokens(String str) {
+        StringTokenizer tokenizer = new StringTokenizer(str);
+        for (int count = 0; tokenizer.hasMoreTokens(); count++) {
+            println("Token #" + count + ": " + tokenizer.nextToken());
+        }
+    }
+
+    public void run() {
+        setFont("Arial-18");
+        String line = readLine("Enter line to tokenize: ");
+        println("The tokens of the string are: ");
+        printTokens(line);
+    }
+}
+
+    
