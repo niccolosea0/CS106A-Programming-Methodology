0b4296333e1a502d4fbd8840cac0a7fbb3133fda
niccolosea0 <niccolosea@gmail.com>
2026-03-02 20:50:51 +0400

    Add CeaserCipher

---
+++ practice/lecture13/CeaserCipher.java
@@ -0,0 +1,36 @@
+import acm.program.*;
+
+public class CeaserCipher extends ConsoleProgram {
+
+    private char encryptChar(char ch, int key) {
+
+        if (Character.isUpperCase(ch)) {
+            ch = (char) ('A' + (ch - 'A' + key) % 26);
+        } else if (Character.isLowerCase(ch)) {
+            ch = (char) ('a' + (ch - 'a' + key) % 26);
+        }
+        return ch;
+    }
+
+    private String encryptCeaser(String str, int key) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            result += encryptChar(ch, key);
+        }
+        return result;
+    }
+
+
+    public void run() {
+       setFont("Arial-18");
+       println("This program uses a Ceaser Cipher for encryption");
+       int key = readInt("Enter encryption key: ");
+       String plainText = readLine("Enter plain text: ");
+       String cipherText = encryptCeaser(plainText, key);
+       println("Cipher text: " + cipherText);
+    }
+}
