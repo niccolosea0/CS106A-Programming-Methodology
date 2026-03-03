72c3d15418f46ff5a86478b19e86acace2a9cad5
niccolosea0 <niccolosea@gmail.com>
2026-03-03 18:31:17 +0400

    Add StringProcessing assignment#1

---
+++ assignments/section assignments/assignment4/src/StringProcessing.java
@@ -0,0 +1,43 @@
+import acm.program.*;
+
+public class StringProcessing extends ConsoleProgram {
+
+    private String addCommasToNumericString(String digits) {
+        
+        int length = digits.length();
+        int count = 1;
+        String string = "";
+        
+        for (int i = length - 1; i >= 0; i--) {
+            if (count % 3 == 0) {
+
+                // Get part of the String before particular index
+                String oneString = digits.substring(0, i);
+
+                // Get part of the String from index to end
+                String secondString = digits.substring(i, digits.length());
+
+                // Update String with 
+                digits = oneString + "," + secondString;
+            }
+
+            count++;
+        }
+
+        return digits;
+    }
+
+    public void run() {
+
+        setFont("Arial-20");
+        while (true) {
+            String digits = readLine("Enter a numeric string: ");
+            if (digits.length() == 0) { 
+                println("Byee");
+                break;
+            }
+
+            println("Updated value: " + addCommasToNumericString(digits));
+        }
+    }
+}
