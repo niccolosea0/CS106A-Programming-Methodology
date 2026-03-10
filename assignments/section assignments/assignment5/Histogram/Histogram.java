639ce31390efa605ff94083ba285e42f7d611cb3
niccolosea0 <niccolosea@gmail.com>
2026-03-10 14:30:17 +0400

    Add readFile() method

--- assignments/section assignments/assignment5/Histogram/Histogram.java
+++ assignments/section assignments/assignment5/Histogram/Histogram.java
@@ -7,8 +7,9 @@ public class Histogram extends ConsoleProgram {
 
     public void run() {
 
+        setFont("Arial-19");
         BufferedReader fileReader = openFile("Please enter a filename: ");
-
+        List<Integer> scoreList = readFile(fileReader);
     }
 
     // Method to open file carefully
@@ -26,5 +27,25 @@ public class Histogram extends ConsoleProgram {
         }
 
         return reader;
+    }
+
+    private List<Integer> readFile(BufferedReader reader) {
+
+        List<Integer> list = new ArrayList<>();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                try {
+                    int score = Integer.parseInt(line.trim());
+                    println("Read integer: " + score);
+                    list.add(score);
+                } catch (NumberFormatException e) {
+                    println("Skiping non-integer line - " + line);
+                }
+            }
+        } catch (IOException ex) {
+            throw new ErrorException(ex);
+        }
+        return list;
     }
 }
