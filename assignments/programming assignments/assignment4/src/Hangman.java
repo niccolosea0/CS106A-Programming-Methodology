ce3b8aa5b2a816b19c55ef4c2f7f3aba8bfe4daf
niccolosea0 <niccolosea@gmail.com>
2026-04-02 13:56:36 +0400

    Moved HangmanLexicon to different .java file

--- assignments/programming assignments/assignment4/src/Hangman.java
+++ assignments/programming assignments/assignment4/src/Hangman.java
@@ -59,7 +59,7 @@ public class Hangman extends ConsoleProgram {
 
        printFinalMessage(word, display);
 
-     }
+    }
 
     private void printFinalMessage(String word, char[] display) {
 
@@ -108,30 +108,6 @@ public class Hangman extends ConsoleProgram {
             }
 
             return letter;
-        }
-    }
-}
-
-class HangmanLexicon {
-    /** Returns the number of words in the lexicon. */
-    public int getWordCount() {
-        return 10;
-    }
-
-/** Returns the word at the specified index. */
-    public String getWord(int index) {
-        switch (index) {
-            case 0: return "BUOY";
-            case 1: return "COMPUTER";
-            case 2: return "CONNOISSEUR";
-            case 3: return "DEHYDRATE";
-            case 4: return "FUZZY";
-            case 5: return "HUBBUB";
-            case 6: return "KEYHOLE";
-            case 7: return "QUAGMIRE";
-            case 8: return "SLITHER";
-            case 9: return "ZIRCON";
-            default: throw new ErrorException("getWord: Illegal index");
         }
     }
 }
